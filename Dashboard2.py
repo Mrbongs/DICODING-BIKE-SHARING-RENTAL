282949d54874fb0be183729af9ccc668e86afe38
Mario Adi Nugroho <95077897+Mrbongs@users.noreply.github.com>
2023-12-07 18:28:26 +0700

    Update Dashboard2.py

--- Dashboard2.py
+++ Dashboard2.py
@@ -48,10 +48,13 @@ st.pyplot(fig)
 st.subheader('Histogram of Casual vs Registered Users')
 fig, ax = plt.subplots()
 
-# Ensure that 'casual' and 'registered' are numeric and available
+# Convert 'casual' and 'registered' to NumPy arrays
 if 'casual' in filtered_data.columns and 'registered' in filtered_data.columns:
-    sns.histplot(filtered_data['casual'], color='blue', label='Casual', kde=True, ax=ax)
-    sns.histplot(filtered_data['registered'], color='red', label='Registered', kde=True, ax=ax)
+    casual_data = filtered_data['casual'].to_numpy()
+    registered_data = filtered_data['registered'].to_numpy()
+    
+    sns.histplot(casual_data, color='blue', label='Casual', kde=True, ax=ax)
+    sns.histplot(registered_data, color='red', label='Registered', kde=True, ax=ax)
 else:
     st.write('Data for casual or registered users is not available')
 
